3c3a8b675b29ee94ed298e773b43fba33ee23c6b
baesoheee <00700id@naver.com>
2022-07-10 13:53:47 +0900

    게시물 작성 기능 구현, 스캐너로 명령어 입력받기

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -4,14 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==program start==");
+		System.out.println("== program start ==");
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("Enter the command : ");
-		String command = sc.next();
-		System.out.println("input command : "+command);
-		command = sc.next();
-		System.out.println("input command : "+command);
+		int lastID = 0;
+		
+		while(true) {
+			System.out.printf("Enter the command : ");
+			String command = sc.nextLine();
+			command = command.trim();    //공백 처리
+			if (command.length()==0) {
+				continue;
+			}							 // 띄어쓰기만 했을 때 위로 다시 올라가기
+			
+			if(command.equals("exit")) {
+				break;
+			}else if(command.equals("article write")) {
+				int id = lastID + 1;
+				lastID = id;   // ID 갱신
+				System.out.printf("title : ");
+				String title = sc.nextLine();
+				System.out.printf("contents : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d post has been created.\n",id);
+				System.out.printf("title : %s\n",title);
+				System.out.printf("body : %s\n",body);
+				
+			}else if(command.equals("article list")) {
+				System.out.println("There are no posts.");
+			}else {
+				System.out.printf("%s is a non-existent command.\n", command);
+				
+			}
+		}
 		sc.close();
-		System.out.println("==program end==");
+		System.out.println("== program end ==");
 	}
 }
